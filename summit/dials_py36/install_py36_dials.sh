8d70a1ebd2f3765ba09e9af8abd21ee5c099b9fa
Billy K. Poon <bkpoon@lbl.gov>
2019-10-28 23:16:11 -0400

    Add script and environment to build DIALS with Python 3.6

---
+++ summit/dials_py36/install_py36_dials.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+# setup compiler
+module load gcc/7.4.0
+
+# variables needed for conda
+export CONDA_PREFIX=$PWD/conda
+
+# Install Conda environment.
+rm -fr $CONDA_PREFIX
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-ppc64le.sh
+bash Miniconda3-latest-Linux-ppc64le.sh -b -p $CONDA_PREFIX
+rm Miniconda3-latest-Linux-ppc64le.sh
+source $CONDA_PREFIX/etc/profile.d/conda.sh
+
+# clean up
+mkdir -p dials
+cd dials
+rm -fr *
+
+# set language to ensure utf-8 encoding
+export LC_ALL=en_US.utf-8
+
+# get bootstrap.py (currently from "migrate" branch so it's faster)
+wget "https://raw.githubusercontent.com/cctbx/cctbx_project/0c932a51be5a517f58c0ff0fdc6e2300410e7d89/libtbx/auto_build/bootstrap.py"
+
+# download sources
+python bootstrap.py hot update --builder=dials
+
+# install dependencies
+python bootstrap.py base --use-conda ../dials_py36_env.txt
+conda activate ./conda_base
+pip install dials-data
+pip install mrcfile
+pip install orderedset
+pip install procrunner
+conda deactivate
+
+# build
+python bootstrap.py build --builder=dials --use-conda --nproc=16
+
+cd ..
